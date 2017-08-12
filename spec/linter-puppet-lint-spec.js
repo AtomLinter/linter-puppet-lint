e0dcaf398ebf69c8e3beab989171d7a17c6aa870
Matt Schuchard <grapheneowns@gmail.com>
2017-08-12 10:22:32 -0400

    updating jasmine tests for Linter v2 API

--- spec/linter-puppet-lint-spec.js
+++ spec/linter-puppet-lint-spec.js
@@ -32,19 +32,17 @@ describe('The puppet-lint provider for Linter', () => {
       atom.workspace.open(errorsPath).then(editor => lint(editor)).then((messages) => {
         expect(messages.length).toBe(2);
 
-        expect(messages[0].type).toBe('error');
         expect(messages[0].severity).toBe('error');
         expect(messages[0].html).not.toBeDefined();
-        expect(messages[0].text).toBe('does_not::exist not in autoload module layout');
-        expect(messages[0].filePath).toBe(errorsPath);
-        expect(messages[0].range).toEqual([[0, 6], [0, 14]]);
+        expect(messages[0].excerpt).toBe('does_not::exist not in autoload module layout');
+        expect(messages[0].location.file).toBe(errorsPath);
+        expect(messages[0].location.position).toEqual([[0, 6], [0, 14]]);
 
-        expect(messages[1].type).toBe('warning');
         expect(messages[1].severity).toBe('warning');
         expect(messages[1].html).not.toBeDefined();
-        expect(messages[1].text).toBe('class not documented');
-        expect(messages[1].filePath).toBe(errorsPath);
-        expect(messages[1].range).toEqual([[0, 0], [0, 5]]);
+        expect(messages[1].excerpt).toBe('class not documented');
+        expect(messages[1].location.file).toBe(errorsPath);
+        expect(messages[1].location.position).toEqual([[0, 0], [0, 5]]);
       }),
     );
   });
